55a5961f032d2d1484356f2af2d8e7469ddee41c
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-10-05 20:46:27 -0400

    chore: Add puppeteer dependency and generate PDF from HTML

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+const puppeteer = require("puppeteer");
 
-console.log(greet("World"));
+async function generatePDFfromHTML({
+  htmlContent,
+  outputPath,
+}: {
+  htmlContent: string;
+  outputPath: string;
+}) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(htmlContent);
+  await page.pdf({ path: outputPath, format: "A4" });
+  await browser.close();
+}
+
+const htmlContent = "<h1>Hello Hannah</h1>";
+
+generatePDFfromHTML({ htmlContent: htmlContent, outputPath: "output.pdf" })
+  .then(() => console.log("PDF Generated"))
+  .catch((err) => console.error(err));
