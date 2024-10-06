fe88391f6c35225c2f34ce0a537eaefc83ced652
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-10-05 23:31:16 -0400

    generatePDFfromHTML function and handle PDF response headers

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,23 @@ const app = express();
 // Parse JSON bodies
 app.use(express.json());
 
-async function generatePDFfromHTML({
-  htmlContent,
-  outputPath,
-}: {
-  htmlContent: string;
-  outputPath: string;
-}) {
+async function generatePDFfromHTML({ htmlContent }: { htmlContent: string }) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(htmlContent);
-  await page.pdf({ path: outputPath, format: "A4" });
+  const pdfbuff = await page.pdf({ format: "A4" });
   await browser.close();
+  return pdfbuff;
 }
-
-// const htmlContent = "<h1>Hello Hannah</h1>";
-
-// generatePDFfromHTML({ htmlContent: htmlContent, outputPath: "output.pdf" })
-//   .then(() => console.log("PDF Generated"))
-//   .catch((err) => console.error(err));
 
 app.post("/generate-pdf", async (req: any, res: any) => {
   try {
-    const {
-      htmlContent,
-      outputPath,
-    }: { htmlContent: string; outputPath: string } = req.body;
-    await generatePDFfromHTML({ htmlContent, outputPath });
-    res.status(201).send({ success: true, message: "PDF Generated" });
+    const { htmlContent }: { htmlContent: string } = req.body;
+    const pdfbuff = await generatePDFfromHTML({ htmlContent });
+    // Set headers for PDF response
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Length", pdfbuff.length);
+    res.status(201).end(pdfbuff);
   } catch (error) {
     res.status(500);
   }
